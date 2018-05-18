d1d9916a44eb75f63f37393a80c8ae3a364bb682
konstantin shnaider <kostyash@bezeqint.net>
2018-05-18 17:51:21 +0300

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchPanelComponent } from './search-panel/search-panel.component';
+import { MoviesContainerComponent } from './movies-container/movies-container.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPanelComponent,
+    MoviesContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
